4507a952a47be931239101d0f5008747ab2dfe7d
Joerenald <joerenald27@gmail.com>
2026-06-18 17:41:20 +0530

    fix deployment issue

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useInView } from "react-intersection-observer";
 import Reveal from "./Reveal";
-import SkillCard from "./SkillCard";
+import SkillCard from "./SkillCard.jsx";
 import { motion } from "framer-motion";
 
 const allSkills = [
